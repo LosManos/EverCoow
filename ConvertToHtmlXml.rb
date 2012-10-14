c1205fd41d0817c5d62595f7f8b35477e324e3f3
Ola Fjelddahl <ola.fjelddahl@gmail.com>
2012-10-14 12:13:17 +0200

    Last iteration.  Rexml messes up the text in the Xml text.

--- ConvertToHtmlXml.rb
+++ ConvertToHtmlXml.rb
@@ -38,27 +38,37 @@ def appendToFile( fileName, theString )
 end
 
 
+# This method creates an XML element for the article
+# and inserts it into the XML document sent as parameter.
 def createNoteElement( elemNote, noteRootOut, markdownStringOut )
 
     # Create a new Note child to later return as the base
     # for the Xml this method creates.
-		noteElem = noteRootOut.createChildElement "Note"
+		noteElem = noteRootOut.createChildElement 'Note'
 
     # Get the title of the EN note and create a new XMl element of it.
 		title = elemNote.elements[ "title" ]
 		titleElem = noteElem.createChildElement( "Title" ).
 			setText( title.text )
 
-    # Get the text of teh EN note and create a new Xml element of it.
+    # Get the text of the EN note and create a new Xml element of it.
 		contentText = elemNote.elements[ "content" ].text
 		# contentText = washOld( contentText )
-    contentElem = noteElem.createChildElement( "Content" ).
+    contentElem = noteElem.createChildElement( 'Content' ).
       setText( contentText )
+
+    # Create the markdown text and insert it into the xmldocument.
+    contentAsXml = REXML::Document.new( contentText )
+    enNoteElement = contentAsXml.elements["en-note"]
+    md = enNoteElementToMarkdown( enNoteElement )
+    mlElem = noteElem.createChildElement( 'MarkdownContent').
+        setText( md )
 
     # Create a new Xml document of the EN note's content
     # and get the text out of it.  Lastly wash it from unwanted
     # html tags.
 		contentAsXml = REXML::Document.new( contentText )
+    contentAsXml.
 		enNoteElement = contentAsXml.elements["en-note"]
     pureText = '## ' + title.text + "\n"
     pureText = pureText + enNoteElementToMarkdown( enNoteElement )
@@ -89,8 +99,37 @@ def createNoteElements( inputDoc, chapterName, rootOut, markdownStringOut )
 		createNoteElement( elemNote, rootOut, noteMarkdownStringOut )
     noteMarkdownArray.push( noteMarkdownStringOut)
 	}
+
+  # Return an array with the articles.  One item per array.
+  mdArticleArrayOut = noteMarkdownArray
+
+  # Return a string with the chapter's header and the articles, formatted as markdown.
   markdownString = markdownString + noteMarkdownArray.join("\n")
   markdownStringOut << markdownString
+end
+
+
+# This method opens an XML file
+# and washes the lines some what
+# and outputs as an Html file.
+# The input file name is hard coded and is output from this code.
+# The output file is an Html file that can be imported into Google docs.
+# The output fils is also named with today's date.
+def convertAndWriteToGoogleDocCompatibleHtmlFile
+	File.open( 'ToGoogleDocs.xml', 'r' ) do
+		|inFile|
+
+		filename = 'CCC' + Time.now.strftime( "%Y%m%d") + ' -'
+
+    puts 'Writing file ' + filename + '.html'
+		File.open( filename + '.html', 'w' ) do
+			|outFile|
+
+			while inLine = inFile.gets
+				outFile.puts washOld( inLine )
+			end
+		end
+	end
 end
 
 
@@ -132,18 +171,14 @@ end
 def convertEnexFile( filename, chapterName, cccommunicateElement, markdownStringOut )
 	xmlInputFile = File.new( filename + ".enex" )
 	xmlInputDoc = Document.new( xmlInputFile )
-  markdownString = ''
+  markdownStringOut = ''
 
 	createNoteElements(
 		xmlInputDoc,
 		chapterName ,
     cccommunicateElement.add_element( "Notebook" ),
-    markdownString
+    markdownStringOut
   )
-
-  markdownStringOut << markdownString
-	#xmlOutputDoc.write( $stdout, 4 )
-
 end
 
 
@@ -151,62 +186,44 @@ end
 def convertEnexFiles
 	xmlOutputDoc = Document.new
 	docRoot = xmlOutputDoc.add_element( "Cccommunicate" )
-  markdownStringOut = ''
+  mdString2PALOut = ''
+  mdString4PSAROut = ''
 
-#	puts( "Converting mb1." )
+#	puts( 'Converting mb1.' )
 #	convertEnexFile( "mb-1-CodeAndDevelopment", "Code and development", docRoot, markdownStringOut )
-  puts( "Converting mb2." )
-  convertEnexFile( "mb-2-ProjectsAndLeadership", "Projects and leadership", docRoot, markdownStringOut )
-# puts( "Converting mb3." )
+  puts( 'Converting mb2.' )
+  convertEnexFile( "mb-2-ProjectsAndLeadership", "Projects and leadership", docRoot, mdString2PALOut )
+# puts( 'Converting mb3.' )
 # convertEnexFile( "mb-3-ProductsAndReleases", "Products and releases", docRoot, markdownStringOut )
-  puts( "Converting mb4." )
-  convertEnexFile( "mb-4-PrivacySecurityAndRights", "Security, privacy and rights", docRoot, markdownStringOut )
-#	puts( "Converting mb5." )
+  puts( 'Converting mb4.' )
+  convertEnexFile( "mb-4-PrivacySecurityAndRights", "Security, privacy and rights", docRoot, mdString4PSAROut )
+#	puts( 'Converting mb5.' )
 #	convertEnexFile( "mb-5-Miscellaneous", "Miscellaneous", docRoot )
 
-	puts( 'Writing xml file CCC.xml.')
+	puts( 'Writing xml file CCC.xml.  This file contains the head lines and contents in XML format.')
 	File.open( "CCC.xml", "w" ){
 		|file|
 		xmlOutputDoc.write( file, 4 )
 	}
 
-  puts( 'Writing markdown file CCC.md.' )
+  puts( 'Writing markdown file CCC.md.  This file contains the articles of the letter as markdown.' )
+  mdString = mdString2PALOut << "\n" << mdString4PSAROut
   outFile = File.open( 'CCC.md', 'w')
-  outFile.write( markdownStringOut )
+  outFile.write( mdString )
   outFile.close()
 end
 
 
-# This method opens an XML file
-# and washes the lines some what
-# and outputs as an Html file.
-# The input file name is hard coded and is output from this code.
-# The output file is an Html file that can be imported into Google docs.
-# The output fils is also named with today's date.
-def convertAndWriteToGoogleDocCompatibleHtmlFile
-	File.open( 'ToGoogleDocs.xml', 'r' ) do
-		|inFile|
-
-		filename = 'CCC' + Time.now.strftime( "%Y%m%d") + ' -'
-
-    puts 'Writing file ' + filename + '.html'
-		File.open( filename + '.html', 'w' ) do
-			|outFile|
-
-			while inLine = inFile.gets
-				outFile.puts washOld( inLine )
-			end
-		end
-	end
+# This method converts a Markdown string to a Html suitable for Email.
+def convertMarkdownToMailHtml( mdString )
+  return '###asdf' + mdString
 end
+
 
 
 # This method takes the Mail template, updates it's contents and outputs a new file.
 # The new file's filename is the parameter.
 def createMailFile( resultFileName )
-
-  dang!  jag har en MD-fil men den har inte kapitel etc som xml.  Antingen bygger jag en enkel
-  md-parser/tolk eller så skriver jag om så jag tillverkar XML med själva artiklarna i MD.
 
   print 'Writing file ' + resultFileName + '...'
   File.open( resultFileName, 'w')do
@@ -214,21 +231,38 @@ def createMailFile( resultFileName )
     outFile.puts( textInFile( 'First.part.html'))
   end
 
+  # Open the XML document we use.
+  xmlInputDoc = Document.new( File.new( "CCC.xml" ) )
+
   print 'leader...'
   leaderPart = textInFile( 'leader.part.html' )
   leaderPart.sub!('{{ledare}}', '*** this is my leader ***')
   appendToFile( resultFileName, leaderPart )
 
-  print 'header...'
-  headerPart = textInFile( 'header.part.html' )
-  headerPart.sub!( '{{header}}', '***MyHEader***')
-  appendToFile( resultFileName, headerPart )
+  # Loop the chapters.
+  XPath.each( xmlInputDoc, "//Cccommunicate/Notebook" ){
+      |notebookElement|
+      chapterTitle = notebookElement.attributes["Title"]
 
-  print 'article...'
-  articlePart = textInFile( 'article.part.html')
-  articlePart.sub!('{{BodyHeader}}', '***this is mh article***')
-  articlePart.sub!( /\{\{Body.*\}\}/, '***this is the body text of the article***' )
-  appendToFile( resultFileName, articlePart )
+      print 'header...'
+      headerPart = textInFile( 'header.part.html' )
+      headerPart.sub!( '{{header}}', chapterTitle )
+      appendToFile( resultFileName, headerPart )
+
+      # Loop the articles.
+      XPath.each( notebookElement, "Note" ){
+        |noteElement|
+
+        articleTitle = noteElement.elements["Title"].text
+        articleMdContent = noteElement.elements["MarkdownContent"].text
+        articleMailHtmlContent = convertMarkdownToMailHtml( articleMdContent )
+        print 'article...'
+        articlePart = textInFile( 'article.part.html')
+        articlePart.sub!('{{BodyHeader}}', articleTitle )
+        articlePart.sub!( /\{\{Body.*\}\}/, articleMailHtmlContent )
+        appendToFile( resultFileName, articlePart )
+      }
+    }
 
   print 'last...'
   appendToFile( resultFileName, textInFile( 'Last.part.html'))
@@ -269,10 +303,11 @@ def enNoteElementToMarkdown( enNoteElement )
 
       washUrlLine( rowArray[index], linkOut, linkCommentOut )
 
-      rowArray[index] = '[' + linkOut + '](' + linkOut + ')' + linkCommentOut
+      rowArray[index] = '- [' + linkOut + '](' + linkOut + ')' + linkCommentOut
     end
   }
-  return rowArray.join( "\n")
+  ret = rowArray.join( "\r\n")
+  return ret
 end
 
 
